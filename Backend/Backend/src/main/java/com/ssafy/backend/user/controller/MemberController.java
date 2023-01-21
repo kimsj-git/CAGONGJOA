2ae9bea600eccdc0a753bf93f27c164ff1245808
devjunmo <devjunmo@gmail.com>
2023-01-21 13:19:43 +0900

    Revert "feat: [BE/EntityMapping] 엔티티 맵핑 진행중"
    This reverts commit c48219284ba97702b9cbb0bf95e902cea1263f7c.

--- Backend/Backend/src/main/java/com/ssafy/backend/user/controller/MemberController.java
+++
@@ -1,4 +0,0 @@
-package com.ssafy.backend.user.controller;
-
-public class MemberController {
-}
--- Backend/Backend/src/main/java/com/ssafy/backend/user/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.backend.user.repository;
-
-import com.ssafy.backend.user.domain.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
