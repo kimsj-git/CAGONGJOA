dcdf3f3cc9f9ac67a4238d79de865c5aa5f4eb95
kimsj <kimsj.py@gmail.com>
2023-01-27 17:39:54 +0900

    feat: [FE/TodayCafe] 오늘의 카페, 커피 내리기, 오늘의 운세 컴포넌트 생성

---
+++ frontend/src/components/todayCafe/Fortune.js
@@ -0,0 +1,9 @@
+import TodayCafe from "../../pages/TodayCafePage"
+
+const Fortune = () => {
+  return <TodayCafe>
+    <h1>오늘의 운세</h1>
+  </TodayCafe>
+}
+
+export default Fortune
---
+++ frontend/src/components/todayCafe/MakeCoffee.js
@@ -0,0 +1,9 @@
+import TodayCafe from "../../pages/TodayCafePage"
+
+const MakeCoffee = () => {
+  return <TodayCafe>
+    <h1>커피 내리기</h1>
+  </TodayCafe>
+}
+
+export default MakeCoffee
