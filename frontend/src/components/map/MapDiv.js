f2de1aa51017a13a5ba5f8c565238b63074dfaf6
조현철 <chaoman5@naver.com>
2023-02-01 17:15:52 +0900

    feat: [FE/Map] Kakao 맵 구현, Backend에서 카페 데이터 가져오는 fetch 구현 필요

--- frontend/src/components/map/MapDiv.js
+++ frontend/src/components/map/MapDiv.js
@@ -1,23 +1,88 @@
 import { useState } from "react"
+import { Map, MapMarker } from "react-kakao-maps-sdk"
+import { Button, Icon } from "semantic-ui-react"
 
-import { Map,MapMarker } from "react-kakao-maps-sdk"
+import classes from './MapDiv.module.css'
+import MapCafeMarker from "./MapCafeMarker"
 import MapCircle from "./MapCircle"
 
 const MapDiv = () => {
-  const [center, setCenter] = useState({lat: sessionStorage.getItem('lat'), lng: sessionStorage.getItem('lng')})
+  const [center, setCenter] = useState({
+    lat: sessionStorage.getItem("lat"),
+    lng: sessionStorage.getItem("lng"),
+  })
+
+  const [isMoved, setIsMoved] = useState(false)
+  const dragHandler = () => {
+    setIsMoved(true)
+  }
+  const goToMyPosition = () => {
+    setCenter({
+      lat: sessionStorage.getItem("lat"),
+      lng: sessionStorage.getItem("lng"),
+    })
+    setIsMoved(false)
+  }
+  // 드래그 했을 때 새로운 카페 정보를 가져오기
+  const DUMMY_DATA = [
+    {
+      lat: center.lat - 0.0003,
+      lng: center.lng + 0.001,
+      name: "스타벅스",
+      crowdValue: "원활",
+      address: "서울시 강남구 역삼역..",
+    },
+    {
+      lat: center.lat - 0.0007,
+      lng: center.lng - 0.0009,
+      name: "할리스",
+      crowdValue: "혼잡",
+      address: "서울시 강남구 테헤란로..",
+    },
+    {
+      lat: center.lat + 0.001,
+      lng: center.lng - 0.003,
+      name: "파스쿠찌",
+      crowdValue: "보통",
+      address: "서울시 강남구 역삼역 1번출구",
+    },
+  ]
   return (
-    <Map
-      center={{ lat: center.lat, lng: center.lng }}
-      style={{ width: "100%", height: "600px" }}
-      level={3}
-      onCenterChanged={(map) => setCenter({
-          lat: map.getCenter().getLat(),
-          lng: map.getCenter().getLng(),
-      })}
-    >
-      <MapMarker position={{ lat: center.lat, lng: center.lng }}/>
-      <MapCircle lat={center.lat} lng={center.lng}/>
-    </Map>
+    <>
+      <Map
+        center={{ lat: center.lat, lng: center.lng }}
+        level={3}
+        onCenterChanged={(map) =>
+          setCenter({
+            lat: map.getCenter().getLat(),
+            lng: map.getCenter().getLng(),
+          })
+        }
+        onDragEnd={dragHandler}
+        className={classes.map}
+      >
+        <MapMarker position={{ lat: center.lat, lng: center.lng }} />
+
+        {DUMMY_DATA.map((cafe, index) => {
+          return (
+            <MapCafeMarker
+              key={index}
+              lat={cafe.lat}
+              lng={cafe.lng}
+              name={cafe.name}
+              crowdValue={cafe.crowdValue}
+              address={cafe.address}
+            />
+          )
+        })}
+        <MapCircle lat={center.lat} lng={center.lng} />
+      {isMoved && (
+        <Button className={classes.myLocationBtn} icon circular onClick={goToMyPosition}>
+          <Icon name="map marker alternate" color="red" />
+        </Button>
+      )}
+      </Map>
+    </>
   )
 }
 
