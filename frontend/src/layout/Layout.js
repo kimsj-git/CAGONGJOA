f6dbe572933255cb9b245529c13346cdf8380c0e
kimsj <kimsj.py@gmail.com>
2023-02-02 10:32:28 +0900

    feat: [FE/Layout] MainNavigation, SideNavigation 반응형 디자인 수정

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -1,27 +1,28 @@
+import { Fragment } from "react"
 import { Container, Grid } from "semantic-ui-react"
 
 import MainNavigation from "./MainNavigation"
 import SideNavigation from "./SideNavigation"
 
-import classes from "./MainNavigation.module.css"
-
 const Layout = (props) => {
   return (
-    <Container>
-      <Grid>
-        <Grid.Column only="computer" width={2}>
-          <SideNavigation />
-        </Grid.Column>
-        <Grid.Column width={14}>
-          <Grid.Row>
-            <main style={{ paddingBottom: "5rem" }}>{props.children}</main>
-          </Grid.Row>
-          <Grid.Row only="mobile tablet">
-            <MainNavigation />
-          </Grid.Row>
-        </Grid.Column>
-      </Grid>
-    </Container>
+    <Fragment>
+      <Container>
+        <Grid>
+          <Grid.Column only="computer" width={2}>
+            <SideNavigation />
+          </Grid.Column>
+          <Grid.Column only="mobile tablet" width={1} />
+          <Grid.Column width={14}>
+            <Grid.Row>
+              <main style={{ paddingBottom: "5rem" }}>{props.children}</main>
+            </Grid.Row>
+          </Grid.Column>
+          <Grid.Column only="mobile tablet" width={1} />
+        </Grid>
+      </Container>
+      <MainNavigation />
+    </Fragment>
   )
 }
 
