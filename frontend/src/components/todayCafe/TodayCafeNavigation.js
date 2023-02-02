5f9d6865e3b8e9c7d92659aee4dab2d2d7104bca
kimsj <kimsj.py@gmail.com>
2023-02-02 14:53:27 +0900

    feat: [FE/TodayCafe] 오늘의 카페 네비게이션 스타일 적용

--- frontend/src/components/todayCafe/TodayCafeNavigation.js
+++ frontend/src/components/todayCafe/TodayCafeNavigation.js
@@ -1,20 +1,38 @@
-import { Fragment } from "react"
+import { useState, Fragment } from "react"
 import { NavLink } from "react-router-dom"
+import { Grid, Container, Button } from "semantic-ui-react"
 
 const TodayCafeNavigation = () => {
+  // const [focused, setFocused] = useState('0')
+
+  // const navigationHandler = (selectedBtn) => {
+  //   if (selectedBtn === '1') {
+  //     setFocused('1')
+  //   }
+  //   else if (selectedBtn === '2') {
+  //     setFocused('2')
+  //   }
+  // }
+
   return (
     <Fragment>
-      <ul>
-        <li>
-          <NavLink to="/today-cafe">현재 카페</NavLink>
-        </li>
-        <li>
-          <NavLink to="/today-cafe/make-coffee">커피 내리기</NavLink>
-        </li>
-        <li>
-          <NavLink to="/today-cafe/fortune">오늘의 운세</NavLink>
-        </li>
-      </ul>
+      <Grid columns={3} divided>
+        <Grid.Column style={{ textAlign: "center" }}>
+          <NavLink to="/today-cafe" >
+            <Button >현재 카페</Button>
+          </NavLink>
+        </Grid.Column>
+        <Grid.Column style={{ textAlign: "center" }}>
+          <NavLink to="/today-cafe/make-coffee" >
+            <Button >커피 내리기</Button>
+          </NavLink>
+        </Grid.Column>
+        <Grid.Column style={{ textAlign: "center" }}>
+          <NavLink to="/today-cafe/fortune" >
+            <Button >오늘의 운세</Button>
+          </NavLink>
+        </Grid.Column>
+      </Grid>
     </Fragment>
   )
 }
--- frontend/src/pages/TodayCafePage.js
+++ frontend/src/pages/TodayCafePage.js
@@ -1,18 +1,26 @@
 import TodayCafeNavigation from "../components/todayCafe/TodayCafeNavigation"
 
+import { Grid, Container } from "semantic-ui-react"
+
 const TodayCafe = (props) => {
   return (
-    <div
+    <Container
       style={{
         backgroundColor: "#f9f9f9",
         borderRadius: "30px 30px 0px 0px",
         padding: "2rem",
-        margin: '5px'
+        margin: "5px",
       }}
     >
-      <TodayCafeNavigation />
-      <main>{props.children}</main>
-    </div>
+      <Grid>
+        <Grid.Column>
+          <Grid.Row style={{marginBottom: '2rem'}}>
+            <TodayCafeNavigation />
+          </Grid.Row>
+          <Grid.Row>{props.children}</Grid.Row>
+        </Grid.Column>
+      </Grid>
+    </Container>
   )
 }
 
