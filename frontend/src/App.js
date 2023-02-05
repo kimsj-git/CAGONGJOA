c6f089098b8a0887da0b4fdd990f4b3bbb315589
devjunmo <devjunmo@gmail.com>
2023-02-05 19:39:23 +0900

    release: front, back docker file 작성 및 젠킨스 파이프라인 수정 + 환경변수 세팅

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,35 @@
-import { useCallback, useEffect } from "react"
-import { Route, Switch } from "react-router-dom"
-import KakaoLoginGetCode from "./components/member/login/KakakLoginGetCode"
-import Layout from "./layout/Layout"
+import { useCallback, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import KakaoLoginGetCode from "./components/member/login/KakakLoginGetCode";
+import Layout from "./layout/Layout";
 
-import LoginPage from "./pages/member/LoginPage"
-import SignupPage from "./pages/member/SignupPage"
-import MainPage from "./pages/MainPage"
-import ChatPage from "./pages/ChatPage"
-import TodayCafe from "./components/todayCafe/TodayCafe"
-import MakeCoffee from "./components/todayCafe/MakeCoffee"
-import Fortune from "./components/todayCafe/Fortune"
-import MyPage from "./pages/MyPage"
-import NotFound from "./pages/NotFound"
+import LoginPage from "./pages/member/LoginPage";
+import SignupPage from "./pages/member/SignupPage";
+import MainPage from "./pages/MainPage";
+import ChatPage from "./pages/ChatPage";
+import TodayCafe from "./components/todayCafe/TodayCafe";
+import MakeCoffee from "./components/todayCafe/MakeCoffee";
+import Fortune from "./components/todayCafe/Fortune";
+import MyPage from "./pages/MyPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
-
   const getLocation = useCallback(() => {
     if (navigator.geolocation) {
       // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-              sessionStorage.setItem('lat', position.coords.latitude) // 위도
-              sessionStorage.setItem('lng', position.coords.longitude) // 경도
-        })}
-    else {
+      navigator.geolocation.getCurrentPosition((position) => {
+        sessionStorage.setItem("lat", position.coords.latitude); // 위도
+        sessionStorage.setItem("lng", position.coords.longitude); // 경도
+      });
+    } else {
       // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-      console.log("X")
-      }
-    }, [])
+      console.log("X");
+    }
+  }, []);
 
   useEffect(() => {
-    getLocation()
-  }, [getLocation])
+    getLocation();
+  }, [getLocation]);
 
   return (
     <Layout>
@@ -67,7 +65,7 @@ function App() {
         </Route>
 
         {/* MainPage Route */}
-        <Route path="/">
+        <Route path="/cagong">
           <MainPage />
         </Route>
 
@@ -77,7 +75,7 @@ function App() {
         </Route>
       </Switch>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
