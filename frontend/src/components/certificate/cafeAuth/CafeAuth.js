8d175d255ce9dbfd7b79be80c405f20c79199af2
양준모 <yajumo93@gmail.com>
2023-02-06 16:20:47 +0900

    release: cafeauth front modify

--- frontend/src/components/certificate/cafeAuth/CafeAuth.js
+++ frontend/src/components/certificate/cafeAuth/CafeAuth.js
@@ -10,7 +10,7 @@ const CafeAuth = () => {
   const open = useSelector((state) => state.modal.openCafeAuthModal)
 
   const nextPageHander = () => {
-    dispatch(findNearCafeData(0.2))
+    dispatch(findNearCafeData())
     dispatch(modalActions.toggleNearCafeListModal())
     dispatch(modalActions.toggleCafeAuthModal())
   }
