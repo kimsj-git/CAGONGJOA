e9ba1906b91df53e34db63017cba27de23f667d8
kaeng2 <6oston.ivy@gmail.com>
2023-02-08 01:13:59 +0900

    feat: [FE/design] 로고 및 커스텀 아이콘 추가

--- frontend/src/pages/member/SignupPage.js
+++ frontend/src/pages/member/SignupPage.js
@@ -1,12 +1,14 @@
-import Signup from "../../components/member/signup/Signup"
+import Signup from "../../components/member/signup/Signup";
+import { Image } from "semantic-ui-react";
 
 const SignupPage = () => {
-    return (
-        <div>
-            <h1>카공조아</h1>
-            <Signup/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Image src="" />
+      <h1 id="title">카공조아</h1>\
+      <Signup />
+    </div>
+  );
+};
 
-export default SignupPage
+export default SignupPage;
