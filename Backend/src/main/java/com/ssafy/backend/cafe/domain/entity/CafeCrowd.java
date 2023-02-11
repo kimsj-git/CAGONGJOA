79c2edcb55ae751f0ff547b1c044f362cbe1ae78
devjunmo <devjunmo@gmail.com>
2023-02-11 20:57:17 +0900

    fix: [BE/CafeCrowd-save] 혼잡도 저장 로직 수정

--- Backend/src/main/java/com/ssafy/backend/cafe/domain/entity/CafeCrowd.java
+++ Backend/src/main/java/com/ssafy/backend/cafe/domain/entity/CafeCrowd.java
@@ -1,9 +1,8 @@
 package com.ssafy.backend.cafe.domain.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import com.ssafy.backend.member.domain.enums.OauthType;
+import lombok.*;
+import org.springframework.util.Assert;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -27,5 +26,13 @@ public class CafeCrowd {
 
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @Builder(builderClassName = "cafeCrowdBuilder", builderMethodName = "cafeCrowdBuilder")
+    public CafeCrowd(Cafe cafe, int crowdValue) {
+        Assert.notNull(cafe, "cafe must not be null");
+
+        this.cafe = cafe;
+        this.crowdValue = crowdValue;
+    }
 
 }
