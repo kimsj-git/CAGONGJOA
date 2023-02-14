587c4debb242f54635b395e626ee7131a567f66d
조현철 <chaoman5@naver.com>
2023-02-14 14:14:55 +0900

    merge: [FE/CafeAuth] 재 로그인 시 cafeAuth 확인하면 sessionStorage에 정보 다시 담기

--- frontend/src/components/myPage/myPost/MyPostsItem.js
+++ frontend/src/components/myPage/myPost/MyPostsItem.js
@@ -3,6 +3,7 @@ import MyPostDetail from './MyPostDetail'
 import classes from "./MyPostsItem.module.css"
 const MyPostsItem = ({ post }) => {
   const content = post.content.length>50 ? `${post.content.substr(0, 50)}...` : post.content
+  
   return (
     <Item className={classes.items}>
       <Item.Image
@@ -18,9 +19,9 @@ const MyPostsItem = ({ post }) => {
       <Item.Content className={classes.content}>
         <Item.Header>[{post.cafeName ? post.cafeName : "질문글"}]</Item.Header>
         <Item.Meta>
-          <span>{content}</span>
+          <span dangerouslySetInnerHTML={{ __html: content }}></span>
         </Item.Meta>
-        <Item.Extra className={classes.detailBtn}>{post.postType}
+        <Item.Extra className={classes.detailBtn} >{post.postType}
         <MyPostDetail post={post}/>
         </Item.Extra>
       </Item.Content>
