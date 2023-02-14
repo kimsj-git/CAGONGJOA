7094b2ffeea97ab476500f3d35484ab0f7fef231
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-02-14 10:49:29 +0900

    feat: [FE/TodayCafe] 현재 카페 정보 반영하도록 수정

--- frontend/src/components/todayCafe/CafeInfo.js
+++ frontend/src/components/todayCafe/CafeInfo.js
@@ -4,12 +4,63 @@ import { BsFillPatchQuestionFill } from "react-icons/bs"
 
 import CafeTimer from "./CafeTimer"
 
-const CafeInfo = () => {
+const BRAND_LOGOS = {
+  할리스: "hollys",
+  폴바셋: "paulbasset",
+  파스쿠찌: "pascucci",
+  투썸플레이스: "twosome",
+  토프레소: "topresso",
+  텐퍼센트커피: "tenpercent",
+  탐앤탐스: "tomntoms",
+  컴포즈커피: "compose",
+  커피에반하다: "coffeebanada",
+  커피스미스: "coffeesmith",
+  커피빈: "coffeebean",
+  커피베이: "coffeebay",
+  커피나무: "coffeenamu",
+  카페베네: "caffeebene",
+  카페띠아모: "caffetiamo",
+  전광수: "jungwang",
+  이디야커피: "edia",
+  요거프레소: "yogerpresso",
+  엔제리너스: "angelinus",
+  스타벅스: "starbucks",
+  스무디킹: "smoothy",
+  셀렉토커피: "selecto",
+  빽다방: "paiksdabang",
+  베스킨라빈스: "baskin",
+  메가커피: "megacoffee",
+  매머드: "mammoth",
+  드롭탑: "droptop",
+  더벤티: "theventi",
+  달콤커피: "dalkomm",
+  나우커피: "nowcoffee",
+  공차: "gongcha",
+  개인카페: "selfcafe",
+  바나프레소: "banapresso",
+}
+
+const CafeInfo = (props) => {
   const cafeAuth = sessionStorage.getItem("cafeAuth")
-  const logo_url = "starbucks.png"
+  const nowCafe = JSON.parse(sessionStorage.getItem("myCafe"))
+  const todayCafe = JSON.parse(sessionStorage.getItem("todayCafe"))
+  let cafeName = ""
+  let logo_url = ""
+  let tierColor = ""
 
-  const cafeName = cafeAuth ? '스타벅스 강남논현점' : '카페가 인증되지 않았습니다.'
-
+  if (nowCafe === null || todayCafe === null) {
+    cafeName = "카페가 인증되지 않았습니다."
+    logo_url = "selfcafe.png"
+  } else {
+    cafeName = nowCafe.cafeName
+    logo_url = `${BRAND_LOGOS[todayCafe.brandType]}.png`
+    tierColor =
+      parseInt(todayCafe.exp / 1000) < 5
+        ? ["#8B6331", "#C0C0C0", "#FF9614", "#3DFF92"][
+            parseInt(todayCafe.exp / 1000)
+          ]
+        : "#65B1EF"
+  }
   return (
     <Container style={{ backgroundColor: "#f9f9f9" }}>
       <Grid>
@@ -24,10 +75,10 @@ const CafeInfo = () => {
                 color="grey"
               />
             )}
-            {cafeAuth === '1' && (
+            {cafeAuth === "1" && (
               <img
                 src={require(`../../assets/cafe_logos/${logo_url}`)}
-                style={{ border: "3vw solid #fbbc05", borderRadius: "70%" }}
+                style={{ border: `3vw solid ${tierColor}`, borderRadius: "70%" }}
                 alt="#"
               />
             )}
@@ -55,12 +106,12 @@ const CafeInfo = () => {
                   color="grey"
                 />
               )}
-              {cafeAuth === '1' && (
+              {cafeAuth === "1" && (
                 <img
                   src={require(`../../assets/cafe_logos/${logo_url}`)}
                   style={{
                     width: "100%",
-                    border: "1vw solid #fbbc05",
+                    border: `1vw solid ${tierColor}`,
                     borderRadius: "70%",
                   }}
                   alt="#"
