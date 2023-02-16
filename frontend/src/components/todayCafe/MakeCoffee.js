c2c926bfde45f25cd884f3783e85d95b765f0c7f
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-02-17 02:48:35 +0900

    feat: [FE/TodayCafe] 커피콩, 커피 개수 fetch 추가

--- frontend/src/components/todayCafe/MakeCoffee.js
+++ frontend/src/components/todayCafe/MakeCoffee.js
@@ -1,31 +1,55 @@
 import { useState, useEffect } from "react"
 import TodayCafePage from "../../pages/TodayCafePage"
 import { Button, Icon } from "semantic-ui-react"
+import useFetch from "../../hooks/useFetch"
 const DEFAULT_REST_URL = process.env.REACT_APP_REST_DEFAULT_URL
 
 const MakeCoffee = () => {
   const cafeAuth = sessionStorage.getItem("cafeAuth")
-  const todayCafe = JSON.parse(sessionStorage.getItem("todayCafe"))
-  let initialCoffeeCnt = 0
-  let initialCoffeeBeanCnt = 0
+  const { data: fetchedCoffee, sendRequest: fetchCoffee } = useFetch()
+  const [coffeeBeanCnt, setCoffeeBeanCnt] = useState(0)
+  const [coffeeCnt, setCoffeeCnt] = useState(0)
 
-  if (todayCafe !== null) {
-    initialCoffeeCnt = todayCafe.coffeeCnt
-    initialCoffeeBeanCnt = todayCafe.coffeeBeanCnt
-  } 
-  const [coffeeCnt, setCoffeeCnt] = useState(initialCoffeeCnt)
-  const [coffeeBeanCnt, setCoffeeBeanCnt] = useState(initialCoffeeBeanCnt)
-
-  const coffeeMakeHandler = async (coffeeMakeType) => {
-    const response = await fetch(`${DEFAULT_REST_URL}/todaycafe/coffeemaking/?coffeeMakeType=${coffeeMakeType}`,{
-      method: 'PUT',
+  useEffect(() => {
+    fetchCoffee({
+      url: `${DEFAULT_REST_URL}/member/coin`,
       headers: {
-        "Content-Type": "application/json",
         Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
       },
     })
+  }, [])
+
+  useEffect(() => {
+    setCoffeeBeanCnt(fetchedCoffee.coffeeBeanCnt)
+    setCoffeeCnt(fetchedCoffee.coffeeCnt)
+  }, [fetchedCoffee])
+
+  // const todayCafe = JSON.parse(sessionStorage.getItem("todayCafe"))
+  // let initialCoffeeCnt = 0
+  // let initialCoffeeBeanCnt = 0
+
+  // if (todayCafe !== null) {
+  //   initialCoffeeCnt = todayCafe.coffeeCnt
+  //   initialCoffeeBeanCnt = todayCafe.coffeeBeanCnt
+  // }
+  // const [coffeeCnt, setCoffeeCnt] = useState(initialCoffeeCnt)
+  // const [coffeeBeanCnt, setCoffeeBeanCnt] = useState(initialCoffeeBeanCnt)
+
+  const coffeeMakeHandler = async (coffeeMakeType) => {
+    const response = await fetch(
+      `${DEFAULT_REST_URL}/todaycafe/coffeemaking/?coffeeMakeType=${coffeeMakeType}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+        },
+      }
+    )
     const responseData = await response.json()
-    if (responseData.httpStatus === 'NOT_ACCEPTABLE') {
+    if (cafeAuth !== "1") {
+      alert("카페 인증 후 커피를 내릴 수 있어요.\uD83D\uDE4F")
+    } else if (responseData.httpStatus === "NOT_ACCEPTABLE") {
       // console.log(responseData)
       alert("커피콩이 부족합니다.")
     } else {
@@ -33,7 +57,11 @@ const MakeCoffee = () => {
       setCoffeeCnt(responseData.data.coffeeCnt)
       setCoffeeBeanCnt(responseData.data.coffeeBeanCnt)
       let todayCafe = JSON.parse(sessionStorage.getItem("todayCafe"))
-      todayCafe = {...todayCafe, coffeeCnt: responseData.data.coffeeCnt, coffeeBeanCnt: responseData.data.coffeeBeanCnt}
+      todayCafe = {
+        ...todayCafe,
+        coffeeCnt: responseData.data.coffeeCnt,
+        coffeeBeanCnt: responseData.data.coffeeBeanCnt,
+      }
       sessionStorage.setItem("todayCafe", JSON.stringify(todayCafe))
     }
   }
